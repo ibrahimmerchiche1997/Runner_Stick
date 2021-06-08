bf29905823d6df4747d67db748392736493d041f
IbrahimTazi <65645808+ibrahimmerchiche1997@users.noreply.github.com>
2021-06-08 23:08:30 +0200

    new files
    dz

---
+++ Boxer Running/Assets/Script/ObstaclsSystem.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class ObstaclsSystem : MonoBehaviour
+{
+
+    public float time;
+    public static bool isPushed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Obstacles_ID(transform.name);
+    }
+
+
+
+    public void Obstacles_ID(string n)
+    {
+        switch (n)
+        {
+            case "Obstacles_MoverX_positive":
+                Obstacles_MoverX(transform);
+                break;
+            case "Obstacles_MoverX_negative":
+                Obstacles_MoverX(transform);
+                break;
+            case "Obstacles_Rotation_Cylinder":
+                Obstacles_Rotation_Cylinder(transform);
+                break;
+            case "Cylinder_upper":
+                Cylinder_upper(transform);
+                break;
+
+
+        }
+    }
+
+    private void Obstacles_MoverX(Transform obs)
+    {
+
+        if (obs.name == "Obstacles_MoverX_positive")
+            obs.DOMoveX(obs.transform.position.x - 3f, time).SetEase(Ease.InSine).SetLoops(-1, LoopType.Yoyo);
+
+        if (obs.name == "Obstacles_MoverX_negative")
+            obs.DOMoveX(obs.transform.position.x + 3f, time).SetEase(Ease.InSine).SetLoops(-1, LoopType.Yoyo);
+
+
+    }
+
+    private void Obstacles_Rotation_Cylinder(Transform obs)
+    {
+        obs.DORotate(new Vector3(0,0,0), time).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+      
+    }
+
+   private void Cylinder_upper(Transform obs)
+    {
+        obs.transform.DOMoveY(6.72f, time).SetLoops(-1,LoopType.Yoyo);
+    }
+
+    private void Obstacles_Door()
+    {
+        if (isPushed)
+        {
+            transform.DOMoveX(transform.position.x - 5, time);
+            //Play audio openning door
+        }
+        else
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                gameObject.AddComponent<Rigidbody>().AddForce(Vector3.forward * .5f);
+            }
+        }
+
+
+    }
+
+
+    public void Push(Transform obs)
+    {
+        isPushed = true;
+        Obstacles_Door();
+        obs.DOMoveY(obs.transform.position.y - 1, .2f);
+        //play audio push
+    }
+
+
+    public void DestroyObstacle(Transform col)
+    {
+        Debug.Log("tt");
+        Vector3 explosionPosition = col.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, 5);
+        foreach (Collider hit in colliders)
+        {
+            Debug.Log("ttsss");
+
+            if (hit.gameObject.tag == "Obstacls")
+            {
+                Debug.Log("rrrr");
+                if (hit.GetComponent<Rigidbody>() == null)
+                {
+                    Debug.Log("tqqqqqt");
+                    hit.gameObject.AddComponent<Rigidbody>().AddExplosionForce(1, explosionPosition, 1, .1f, ForceMode.Impulse);
+                    Debug.Log(hit.name);
+                }
+                hit.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1, explosionPosition, 1, .1f, ForceMode.Impulse);
+            }
+
+
+
+        }
+    }
+}
---
+++ Boxer Running/Assets/Script/Player_Movement.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public ObstaclsSystem os;
+
+    public CharacterController controller;
+    Touch touch;
+
+    //Movement
+    public float _speed_Movement_X = 2f;
+    public float _speed_Movement_forward = 2f;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public Transform _GroundCheck;
+    public float _raduisCheckGround = .4f;
+    public LayerMask Groundmask;
+    bool isGrounded;
+    Vector3 moving;
+    public float jumpHeight = 3;
+    public float t;
+    public float le;
+    public Camera main;
+
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Camera follower system
+        if (transform.position.x < 2f)
+            main.transform.position = Vector3.Lerp(main.transform.position, new Vector3(2f, transform.position.y+7 , transform.position.z+t), le * Time.deltaTime);
+        else if (transform.position.x > 20)
+            main.transform.position = Vector3.Lerp(main.transform.position, new Vector3(20f, transform.position.y+7 , transform.position.z +t), le * Time.deltaTime);
+        else
+            main.transform.position = Vector3.Lerp(main.transform.position, new Vector3(transform.position.x, transform.position.y + 7, transform.position.z + t), le * Time.deltaTime);
+
+        //check every frame if player is on ground or not
+        isGrounded = Physics.CheckSphere(_GroundCheck.position, _raduisCheckGround, Groundmask);
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+
+        //Move the player in forward direction automatically
+        controller.Move(transform.forward * _speed_Movement_forward * Time.deltaTime);
+
+
+        //Move the player in x axis
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+
+                float x = touch.deltaPosition.x * Time.deltaTime;
+                moving = Vector3.right * x;
+                controller.Move(moving * _speed_Movement_X * Time.deltaTime);
+
+
+            }
+        }
+
+        //smooth fall to the ground
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+    //Jump system
+    public void JumpSystem()
+    {
+        if (isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Finish") JumpSystem();
+
+        if (other.gameObject.tag == "Obstacls")
+        {
+            // call obstacles system method
+            os.DestroyObstacle(other.transform);
+            StartCoroutine(SlowMotion());
+        }
+    }
+
+
+
+
+    //Slow motion Effect
+    IEnumerator SlowMotion()
+    {
+        int timescaling = 1;
+        while (timescaling > 0)
+        {
+            Time.timeScale = 0.2f;
+            yield return new WaitForSeconds(.2f);
+            Time.timeScale = 1f;
+            Debug.Log("gg");
+            timescaling--;
+        }
+
+    }
+}
+
+
+
+
